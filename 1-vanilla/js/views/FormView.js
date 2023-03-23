829a0c1e595fc69c3277e4a40fd04c63dc82bf63
chaeha293@naver.com <chaeha293@naver.com>
2023-03-23 10:17:30 +0900

    feat: Section 2. 검색폼 > 검색폼 구현 2

--- 1-vanilla/js/views/FormView.js
+++ 1-vanilla/js/views/FormView.js
@@ -5,15 +5,24 @@ const tag = '[FormView]'
 const FormView = Object.create(View)
 
 FormView.setup = function (el) {
-  this.init(el)
-  this.inputEl = el.querySelector('[type=text]')
-  this.resetEl = el.querySelector('[type=reset')
-  this.showResetBtn(false)
-  return this
+    this.init(el)
+    this.inputEl = el.querySelector('[type=text]')
+    this.resetEl = el.querySelector('[type=reset')
+    this.showResetBtn(false)
+    this.bindEvents()
+    return this
 }
 
 FormView.showResetBtn = function (show = true) {
-  this.resetEl.style.display = show ? 'block' : 'none'
+    this.resetEl.style.display = show ? 'block' : 'none'
+}
+
+FormView.bindEvents = function () {
+    this.inputEl.addEventListener('keyup', e => this.onKeyup(e))
+}
+
+FormView.onKeyup = function () {
+    this.showResetBtn(this.inputEl.value.length)
 }
 
 export default FormView
