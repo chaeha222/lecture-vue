57f4a1e2c25a9b6e74715fae4ab3940b67d349a0
chaeha293@naver.com <chaeha293@naver.com>
2023-03-23 10:27:54 +0900

    feat: Section 2. 검색폼 > 검색폼 구현 2

--- 1-vanilla/js/views/FormView.js
+++ 1-vanilla/js/views/FormView.js
@@ -5,25 +5,28 @@ const tag = '[FormView]'
 const FormView = Object.create(View)
 
 FormView.setup = function (el) {
-  this.init(el)
-  this.inputEl = el.querySelector('[type=text]')
-  this.resetEl = el.querySelector('[type=reset')
-  this.showResetBtn(false)
-  this.bindEvents()
-  return this
+    this.init(el)
+    this.inputEl = el.querySelector('[type=text]')
+    this.resetEl = el.querySelector('[type=reset')
+    this.showResetBtn(false)
+    this.bindEvents()
+    return this
 }
 
 FormView.showResetBtn = function (show = true) {
-  this.resetEl.style.display = show ? 'block' : 'none'
+    this.resetEl.style.display = show ? 'block' : 'none'
 }
 
 FormView.bindEvents = function () {
-  this.on('submit', e => e.preventDefault())
-  this.inputEl.addEventListener('keyup', e => this.onKeyup(e))
+    this.on('submit', e => e.preventDefault())
+    this.inputEl.addEventListener('keyup', e => this.onKeyup(e))
 }
 
 FormView.onKeyup = function (e) {
-  this.showResetBtn(this.inputEl.value.length)
+    const enter = 13
+    this.showResetBtn(this.inputEl.value.length)
+    if (e.keyCode !== enter) return
+    this.emit('@submit', { input: this.inputEl.value })
 }
 
 export default FormView
